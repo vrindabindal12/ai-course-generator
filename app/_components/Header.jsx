78e7f5b1f119817ef2402c6d4a0020d06e39606e
vrindabindal12 <vrindabindal1212@gmail.com>
2026-06-27 00:08:51 +0530

    theeme

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
-import { ModeToggle } from '@/components/ui/ModeToggle'
 
 const Header = () => {
   return (
     <div className=' p-5 flex  justify-between shadow-md'>
         <Image src={'/logo.png'} width={150} height={100}/>
         <div className='flex gap-2'>
-            <ModeToggle />
             <Button>Get Started</Button>
         </div>
        
--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -2,19 +2,17 @@
 import { UserButton } from '@clerk/nextjs';
 import React from 'react'
 import { HiAcademicCap } from "react-icons/hi";
-import { ModeToggle } from '@/components/ui/ModeToggle'
 
 const Header = () => {
   return (
     <div className='flex justify-between items-center px-8 py-4 border-b border-neutral-900 bg-black/80 backdrop-blur-md sticky top-0 z-40 text-[#E1E0CC]'>
-        <div className="flex items-center gap-2">
-            <HiAcademicCap className="text-xl text-[#DEDBC8]" />
-            <span className="text-xs font-mono tracking-widest uppercase text-neutral-500">Workspace Panel</span>
-        </div>
-        <div className='flex items-center gap-4'>
-            <ModeToggle />
-            <UserButton afterSignOutUrl="/"/>
-        </div>
+      <div className="flex items-center gap-2">
+
+        <span className="text-xs font-mono tracking-widest uppercase text-neutral-500">Workspace Panel</span>
+      </div>
+      <div className='flex items-center gap-4'>
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   )
 }
